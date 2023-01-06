0a6fa16c729546f88eb7f196c62d119fe7d65f0f
Igor Teplych <igorvrt@mail.ru>
2023-01-07 02:06:53 +0600

    Добавил реализацию декартового дерева

--- Decart/Program.cs
+++ Decart/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Decart;
+
+public class Prorgamm
+{
+    static Random random = new Random();
+    public static void Main()
+    {
+        Treap treap = new Treap(new Node { Key = 20, Priority = random.Next()});
+        treap.Root = treap.Add(25);
+        treap.Root = treap.Add(26);
+        treap.Root = treap.Add(27);
+        treap.Root = treap.Add(28);
+        treap.Root = treap.Add(29);
+        treap.Root = treap.Add(30);
+    }
+}
---
+++ Decart/Treap.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decart
+{
+    public class Treap
+    {
+        Node _root;
+        public Treap(Node root)
+        {
+            _root = root;
+        }
+        public Node Root
+        {
+            get { return _root; }
+            set { _root = value; }
+        }
+
+        void Split(int key, Node root, ref Node left, ref Node right)
+        {
+            Node tree = null;
+            if (root.Key < key)
+            {
+                if (root.Right == null)
+                    right = null;
+                else
+                    Split(key, root.Right, ref tree, ref right);
+                left = new Node(root.Key, root.Priority, root.Left, tree);
+            }
+            else
+            {
+                if (root.Left == null)
+                    left = null;
+                else
+                    Split(key, root.Left, ref left, ref tree);
+                right = new Node(root.Key, root.Priority, tree, root.Right);
+            }
+        }
+        public Node Merge(Node left, Node right)
+        {
+            if (left == null) return right;
+            if (right == null) return left;
+
+            if (left.Priority > right.Priority)
+            {
+                var newR = Merge(left.Right, right);
+                return new Node(left.Key, left.Priority, left.Left, newR);
+            }
+            else
+            {
+                var newL = Merge(left, right.Left);
+                return new Node(right.Key, right.Priority, newL, right.Right);
+            }
+        }
+        static Random random = new Random();
+        public Node Add(int key)
+        {
+            Node left = null;
+            Node right = null;
+            Split(key, Root, ref left, ref left);
+            Node newNode = new Node(key, random.Next());
+            return Merge(Merge(left, newNode), right);
+        }
+    }
+}
